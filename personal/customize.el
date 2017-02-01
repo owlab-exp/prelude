fabc16438d603e5e29785654378b0ca30d1e74c5
Hun Jae Lee <hunjae.lee@gmail.com>
2017-02-01 12:56:02 +0900

    window frame resize

--- personal/customize.el
+++ personal/customize.el
@@ -1,3 +1,3 @@
-(add-to-list 'default-frame-alist '(height . 48))
-(add-to-list 'default-frame-alist '(width . 120))
+(add-to-list 'default-frame-alist '(height . 36))
+(add-to-list 'default-frame-alist '(width . 100))
 (evil-mode 0)
