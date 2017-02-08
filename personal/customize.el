f7ce04fa4c6a14f9394723bfc4cbe844458ef7f0
Hun Jae Lee <hunjae.lee@gmail.com>
2017-02-08 18:28:57 +0900

    web-mode

--- personal/customize.el
+++ personal/customize.el
@@ -1,3 +1,4 @@
-(add-to-list 'default-frame-alist '(height . 36))
+(add-to-list 'default-frame-alist '(height . 72))
 (add-to-list 'default-frame-alist '(width . 100))
 ;;(evil-mode 0)
+(add-to-list 'auto-mode-alist '("\\.boot\\'" . clojure-mode))
