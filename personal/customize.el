fc6e1b3d486c4270b2db86f91b8482ee1cf0ff55
ernest <hunjae.lee@gmail.com>
2017-02-26 00:18:15 +0900

    Fix Java indent to 2 spaces

--- personal/customize.el
+++ personal/customize.el
@@ -3,3 +3,14 @@
 ;;(evil-mode 0)
 (add-to-list 'auto-mode-alist '("\\.boot\\'" . clojure-mode))
 (setq linum-format "%4d \u2502 ")
+;; for frame navigation
+;;(global-set-key (kbd "C-x <up>") 'windmove-up)
+;;(global-set-key (kbd "C-x <down>") 'windmove-down)
+;;(global-set-key (kbd "C-x <right>") 'windmove-right)
+;;(global-set-key (kbd "C-x <left>") 'windmove-left)
+
+;; Java indent
+(add-hook 'java-mode-hook (lambda ()
+                                  (setq c-basic-offset 2
+                                        tab-width 4
+                                        indent-tabs-mode nil)))
